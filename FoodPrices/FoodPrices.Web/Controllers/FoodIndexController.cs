872683f7eb6cebf498ba7674f84f5b71e2b396b4
Gabriel <you@example.com>
2023-07-03 17:30:11 +0100

    initial scafolding

---
+++ FoodPrices/FoodPrices.Web/Controllers/FoodIndexController.cs
@@ -0,0 +1,21 @@
+﻿using FoodPrices.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodPrices.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodIndexController : ControllerBase
+    {
+        /// <summary>
+        /// Return all food indices
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodIndexDto>>> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FoodPrices/FoodPrices.Web/Models/FoodIndexDto.cs
@@ -0,0 +1,35 @@
+﻿namespace FoodPrices.Web.Models
+{
+    public class FoodIndexDto
+    {
+        public FoodIndexDto(string id, string description, DateTimeOffset quoteDate, decimal highPrice)
+        {
+            Id = id;
+            Description = description;
+            QuoteDate = quoteDate;
+            HighPrice = highPrice;
+        }
+
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        /// <example>4915</example>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Description of index
+        /// </summary>
+        /// <example>Urner Barry Beef Index</example>
+        public string Description { get; set; }        
+
+        /// <summary>
+        /// The date of the quote
+        /// </summary>
+        public DateTimeOffset QuoteDate { get; set; }
+
+        /// <summary>
+        /// Highest price
+        /// </summary>
+        public decimal HighPrice { get; set; }
+    }
+}
